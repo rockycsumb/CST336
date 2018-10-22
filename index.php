b8a64a4329c18d94523267ecdfebafec54d29033
Rocky Moreno <romoreno@csumb.edu>
2018-10-22 17:22:47 +0000

    added marks

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo "Rocky Moreno CST336 home page";
+echo "Rocky Moreno CST336 home page!!!";
 
 ?>
 </body>
